36ad3f24ee8b2e9e47160e20b6864546c683bc31
brandonmckinley <brandon.s.mckinley@gmail.com>
2025-04-26 18:59:57 -0500

    working on subclasses scout drone and attack drone

---
+++ attack.h
@@ -0,0 +1,16 @@
+//
+// Created by Brandon McKinley on 4/26/25.
+//
+
+#ifndef ATTACK_H
+#define ATTACK_H
+
+
+
+class attack {
+
+};
+
+
+
+#endif //ATTACK_H
---
+++ scout.h
@@ -0,0 +1,16 @@
+//
+// Created by Brandon McKinley on 4/26/25.
+//
+
+#ifndef SCOUT_H
+#define SCOUT_H
+
+
+
+class scout {
+
+};
+
+
+
+#endif //SCOUT_H
