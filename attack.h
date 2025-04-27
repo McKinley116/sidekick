1ea7047c1bd594e0778afa125027f8e08ac48f42
brandonmckinley <brandon.s.mckinley@gmail.com>
2025-04-26 21:44:34 -0500

    working on attack subclass

--- attack.h
+++ attack.h
@@ -5,11 +5,21 @@
 
 #ifndef ATTACK_H
 #define ATTACK_H
+#include "drone.h"
 
+class attack_drone : public Drone {
+    public:
+      attack_drone(const std::string &string) : Drone(string){}
 
-
-class attack {
-
+      void attack_target(int x, int y) {
+        if (battery_status() > 20) {
+          std::cout << "Attack Drone" << get_name() << " is attacking target at position " << x << "(x), " << y << "(y)...\n";
+          //Remove target from map
+         // Map::remove_target(x, y);
+        }else{
+          std::cout << "Battery too low to attack. Please charge." << std::endl;
+        }
+      }
 };
 
 
