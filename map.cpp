85e8e2bde130c43696195106f60b2227dff26e3a
brandonmckinley <brandon.s.mckinley@gmail.com>
2025-04-29 13:43:55 -0500

    fixing map functions to work with unordered_map

--- map.cpp
+++ map.cpp
@@ -15,26 +15,23 @@ Map::Map() {
 }
 
 // Check if coordinates are within map bounds
-bool Map::is_within_bounds(int x, int y) const {
-    return x >= 0 && x < width && y >= 0 && y < height;
-}
+//bool Map::is_within_bounds(int x, int y) const {
+    //return x >= 0 && x < width && y >= 0 && y < height;
+//}
 
 // Add a drone to the map
 bool Map::add_drone(const Drone& drone) {
-    for (const auto& d : drones) {
-        if (d.get_name() == drone.get_name()) {
-            return false;  // Drone with this name already exists
-        }
-    }
-    drones.push_back(drone);
+   if (drones.find(drone.get_name()) == drones.end()) {
+       return false;
+   }
+    drones [drone.get_name()] = drone;
     return true;
 }
 
 // Move a drone to the specified coordinates
 bool Map::move_drone(const std::string& name, int x, int y) {
     for (auto& d : drones) {
-        if (d.get_name() == name) {
-            d.move(x, y);
+        if (d.name == nameget_name        d.move(x, y);
             return true;
         }
     }
@@ -44,8 +41,8 @@ bool Map::move_drone(const std::string& name, int x, int y) {
 // Charge a drone by name
 bool Map::charge_drone(const std::string& name) {
     for (auto& d : drones) {
-        if (d.get_name() == name) {
-            d.charge();
+        if (d.name == name) {
+  get_name  d.charge();
             return true;
         }
     }
@@ -60,8 +57,8 @@ void Map::display_map() const {
 // Display the status of a specific drone
 bool Map::display_drone_status(const std::string& name) const {
     for (const auto& d : drones) {
-        if (d.get_name() == name) {
-            d.status();
+        if (d.name == name) {
+        get_nameatus();
             return true;
         }
     }
