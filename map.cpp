1d8bef81635f8184133d84ee85a8fcd94b4e8138
Mckinley116 <118803690+McKinley116@users.noreply.github.com>
2025-05-04 22:07:34 -0700

    V2 adding random map size generation with random and ctime library

--- map.cpp
+++ map.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "map.h"
+#include <random>
+#include <ctime>
+
+//generate map function randomly generates the map size and object placement
+void generate_map() {
+    std::random_device rd; // obtains seed for random engine
+    std::mt19937 gen(rd()); // random engine
+    std::uniform_int_distribution<> dis(10, 40); // distribution of random numbers, (min, max)
+    int map_width = dis(gen);
+    int map_height = dis(gen);
+}
+
+// gets number on drones on map
+
+// gets number of objects on the map
--- map.h
+++ map.h
@@ -1,14 +1,21 @@
 //
 // Created by Brandon McKinley on 5/4/25.
-//
+//will store all the functions to generate the map and place drones
 
 #ifndef MAP_H
 #define MAP_H
 
-
-
 class map {
 
+   private:
+    int map_width;
+    int map_height;
+    int drone_count;
+    int object_count;
+    void generate_map();
+    void generate_objects();
+    void get_drone_count();
+    void get_object_count();
 };
 
 
