079c5f363f1552a5610c3ca8d109e12149dec88b
Mckinley116 <118803690+McKinley116@users.noreply.github.com>
2025-05-09 20:00:02 -0700

    working on scout.h/scout.cpp scan function

--- version_2/fighter.cpp
+++ version_2/fighter.cpp
@@ -3,3 +3,6 @@
 //
 
 #include "fighter.h"
+
+//the scan target allows the fighter drone to scan an object that a scout drone must first determine idetification, then determines what kind of munitions to use; standard, heavy, burst.
+
--- version_2/scout.cpp
+++ version_2/scout.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "scout.h"
+
+#include <iostream>
+#include <ostream>
+
+#include "map.h"
+
+// the scan target function allows the scout
+// drone to determine what an object is and what kind of minutitons the fighter drones needs to destryo it
+void scout::scan_target(int x, int y)
+{
+    if (grid[x][y] == EMPTY)
+    {
+        cout << "This gird coordinate is empty" << endl;
+    } else if (grid[x][y] == OBJECT)
+    {
+        cout << "This gird coordinate is an object" << endl;
+    } else if (grid[x][y] == DRONE)
+    {
+        cout << "This gird coordinate is a drone" << endl;
+    } else
+        cout << "coordinates do not exsist within the map" << endl;
+}
