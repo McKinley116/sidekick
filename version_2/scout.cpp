53f00450d27ce9651c36dd8350a805a69ccc22c5
Mckinley116 <118803690+McKinley116@users.noreply.github.com>
2025-05-10 08:07:58 -0700

    fixed scout.cpp scan_target to determine the object randomly and then actually store that object if it is not empty or a friendly drone

--- version_2/scout.cpp
+++ version_2/scout.cpp
@@ -10,41 +10,48 @@
 
 // the scan target function allows the scout
 // drone to determine what an object is and what kind of minutitons the fighter drones needs to destryo it
-void scout::scan_target(int x, int y)
-{
-    if (scan_charges <= 0)
-    {
-        std::cout << "No chargesd left! Please charge drone!" << std::endl;
-        break;
+void scout::scan_target(int x, int y) {
+    if (scan_charges <= 0) {
+        std::cout << "No charges left! Please charge drone!" << std::endl;
+        return;
     }
 
-    std:random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 2);
+    if (!gameMap.is_within_bounds(x, y)) {
+        std::cout << "Target coordinates are outside map boundaries!" << std::endl;
+        return;
+    }
 
     std::cout << "Scanning target at (" << x << ", " << y << ")..." << std::endl;
-    std::cout << "Scan charges left: " << scan_charges << std::endl;
-
-    if (gameMap.grid[x][y] == map::EMPTY)
-    {
-        int object_type = dis(gen);
-
-        switch (object_type)
-        {
-        case 0:
-            std::cout << "Detected: Enemy Drone!\n";
-            break;
-        case 1:
-            std::cout << "Detected: Building\n";
-            break;
-        case 2:
-            std::cout << "Detected: Blockade\n";
-            break;
-
+    
+    if (gameMap.grid[y][x] == map::OBJECT) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, 2);
+        
+        map::object_type detected_type;
+        switch (dis(gen)) {
+            case 0:
+                detected_type = map::ENEMY_DRONE;
+                std::cout << "Detected: Enemy Drone!\n";
+                break;
+            case 1:
+                detected_type = map::BUILDING;
+                std::cout << "Detected: Building\n";
+                break;
+            case 2:
+                detected_type = map::BLOCKADE;
+                std::cout << "Detected: Blockade\n";
+                break;
         }
+        
+        gameMap.set_object_type(x, y, detected_type);
+        scan_charges--;
+        std::cout << "Scan charges left: " << scan_charges << std::endl;
     }
-    else if (gameMap.grid[y][x] == map::DRONE)
-    {
+    else if (gameMap.grid[y][x] == map::EMPTY) {
+        std::cout << "This location is empty.\n";
+    }
+    else if (gameMap.grid[y][x] == map::DRONE) {
         std::cout << "Detected: Friendly Drone\n";
     }
 }
