7c1415cc5871bd3c441864e5eb38343241edb331
Mckinley116 <118803690+McKinley116@users.noreply.github.com>
2025-05-10 09:07:06 -0700

    removed get functions

--- version_2/drone.cpp
+++ version_2/drone.cpp
@@ -14,23 +14,6 @@ void drone::set_position(int x, int y) {
     position_y = y;
 }
 
-std::string drone::get_name() {
-    return name;
-}
-
-int drone::get_id() {
-    return id;
-}
-
-int drone::get_battery() {
-    return battery;
-}
-
-void drone::get_position() {
-    // Implementation needed
-    // You might want to modify this to return or output the position
-}
-
 void drone::move(int x, int y) {
     // Implementation needed
     position_x = x;
