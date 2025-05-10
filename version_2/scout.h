17e534fdf0eeaa313f73a90eef01d4505bf002ff
Mckinley116 <118803690+McKinley116@users.noreply.github.com>
2025-05-10 09:06:12 -0700

    added override for functions to allow for overriding a virtual function from the base class

--- version_2/scout.h
+++ version_2/scout.h
@@ -12,13 +12,11 @@
 // This class inherites from the drone class, and has its own function that allows it to determine what an object that is scaned and gather data on it
 class scout : public drone {
 
-    scout (const std::string &name, int id, map& gameMap) : drone(name, id), gameMap(gameMap)
-    {
-        vision_range = 6;
-        scan_charges = 10;
-    }
+    scout(const std::string &name, int id, map& gameMap)
+      : drone(name, id), gameMap(gameMap), vision_range(6), scan_charges(10) {}
 
-    void scan_target(int x, int y) {} // i want this to determine what type of object it found (drone, building, blockade)
+    const char* get_drone_type() const override { return "Scout"; }
+    void scan_target(int x, int y) override{} // i want this to determine what type of object it found (drone, building, blockade)
     int get_scan_charges() const {}
     int get_range() const {}
 
