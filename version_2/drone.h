142b2cf5d8880e97d174cdf492c841140d18ed02
Mckinley116 <118803690+McKinley116@users.noreply.github.com>
2025-05-10 09:03:17 -0700

    changed drone.h to have virtual functions to allow for polymorphism?

--- version_2/drone.h
+++ version_2/drone.h
@@ -1,40 +1,28 @@
 #ifndef DRONE_H
 #define DRONE_H
+
 #include <string>
 
 class drone {
 public:
-
-    enum missle_type // stores the types of missles that the fighter drone has access to
-    {
-        STANDARD = 0,
-        HEAVY = 1,
-        BURST = 2,
-    };
-
-
-    //Constructor
-    drone(const std::string &name, int id) {
-        this->name = name;
-        this->id = id;
-        this->battery = 100;
-        this->position_x = 0;
-        this->position_y = 0;
-    }
-
-    void move(int x, int y);
-    void set_name(const std::string& name);
-    void set_id(int id);
-    void set_position(int x, int y);
-    std::string get_name();
-    int get_id();
-    int get_battery();
-    void get_position();
+    drone(const std::string& name, int id) 
+        : name(name), id(id), battery(100), position_x(0), position_y(0) {}
     
+    virtual ~drone() = default;  // Virtual destructor
+    virtual void scan_target(int x, int y) = 0;  // Pure virtual function
+    virtual const char* get_drone_type() const = 0;  // Pure virtual function for type identification
+
+    void set_name(const std::string& new_name);
+    void set_id(int new_id);
+    void set_position(int x, int y);
+    std::string get_name() const { return name; }
+    int get_id() const { return id; }
+    int get_battery() const { return battery; }
     int get_position_x() const { return position_x; }
     int get_position_y() const { return position_y; }
+    void move(int x, int y);
 
-private:
+protected:
     std::string name;
     int id;
     int battery;
@@ -42,4 +30,4 @@ private:
     int position_y;
 };
 
-#endif //DRONE_H
+#endif // DRONE_H
